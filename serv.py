7c2a3028112252f681f59cc45a81df95c88026a7
Faiz Khan <faizkhan00@gmail.com>
2015-01-09 10:26:40 -0600

    Fix dumps bug

--- serv.py
+++ serv.py
@@ -58,7 +58,7 @@ def extra(ex='index.html'):
 
 def sanitize(s):
   print([type(s), s])
-  return binascii.hexlify( s.encode('ascii',errors='ignore') ) #.zfill(128)[:128] #max 64 bytes of data allowed
+  return binascii.hexlify( s.encode('ascii',errors='ignore') ).decode('ascii') #.zfill(128)[:128] #max 64 bytes of data allowed
 
 @app.route("/execute", methods=['POST'])
 def execute():
