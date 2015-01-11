b8feb280dfaa758a309b2213d8375d6379592605
Faiz Khan <faizkhan00@gmail.com>
2015-01-10 21:38:55 -0600

    fix more shit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -657,7 +657,8 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
         url: "users",
         success: function(d) {
             var temp = "";
-            console.log(d, typeof d);
+            console.log(d);
+            d=JSON.parse(d);
             d.forEach(function(e) {
               temp += "<tr> <th scope='row'>" + e[0] + "</th> <td>" + e[2] + " </td> <td> " + e[1] + "</td> <td> " + e[3] + "</tr>";
             });
