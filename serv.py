2a58232a254612a70d0f8a7a93c38eed1517bd7c
Faiz Khan <faizkhan00@gmail.com>
2015-01-15 17:56:40 -0600

    uncommitted change?

--- serv.py
+++ serv.py
@@ -140,7 +140,7 @@ def execute():
 
 def decoderow(tup):
   each = tup
-  return (str(int(each[0])), each[1], binascii.unhexlify(each[2]).decode('ascii') if each[2] is not None else None, binascii.unhexlify(each[3]).decode('ascii') )
+  return (int(each[0]), each[1], binascii.unhexlify(each[2]).decode('ascii') if each[2] is not None else None, binascii.unhexlify(each[3]).decode('ascii') )
 
 @app.route("/users")
 def getusers():
