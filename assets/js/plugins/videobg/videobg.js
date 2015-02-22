70ced4b313fe395aff614274f0ff47360ce93c4b
Faiz Khan <faizkhan00@gmail.com>
2015-02-21 22:04:03 -0600

    Fix attempt #2

--- assets/js/plugins/videobg/videobg.js
+++ assets/js/plugins/videobg/videobg.js
@@ -4,20 +4,18 @@ $(document).ready(function() {
     
     //Custom Background Video
     var videobackground = new $.backgroundVideo($('#top'), {
-    "align": "centerXY",
-    "width": 1280,
-    "height": 720,
-    "path": "assets/images/video/",
-    "filename": "blueplexus.mp4",
-    "types": ["mp4"],
-	"loop": true,
-	});
-
+      "align": "centerXY",
+      "width": 1280,
+      "height": 720,
+      "path": "assets/images/video/",
+      "filename": "blueplexus.mp4",
+      "types": ["mp4", "webm"],
+      "loop": true,
+    });
 
     //Youtube Background Video
     $(function(){
       $(".player").mb_YTPlayer();
     });
-
 
 });
